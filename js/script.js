dbaf81a1dee7923e054fbc48aa0f22e9e6cfbece
Mladja <mladenlukic111@gmail.com>
2024-08-26 20:28:02 +0200

    Fixed functionality of the buttons.

--- js/script.js
+++ js/script.js
@@ -10,43 +10,54 @@ addToCartBtns.forEach( button => {
 
         button.innerHTML = `<img src="../assets/images/icon-decrement-quantity.svg" alt="" class="decrement"/>
                             <span class="quantity">1</span>
-                            <img src="../assets/images/icon-increment-quantity.svg" alt="" class="decrement"/>`;
+                            <img src="../assets/images/icon-increment-quantity.svg" alt="" class="increment"/>`;
                             
         const gridItem = button.closest('.grid-item');
         const dessertName = gridItem.querySelector('.dessert-name').textContent;
+        const dessertPrice = gridItem.querySelector('.price').textContent;
 
-        // Add the dessert to cart array
-        cart.push(dessertName);
+        // Checking if item exist in the cart
+        const checkItem = cart.findIndex(item => item.name === dessertName);
 
-        // Update Cart UI
-        updateCartUI();
-    
-        console.log(`Added ${dessertName} to the cart!`);
+        if(checkItem !== -1) {
+            cart[checkItem].quantity++;
+        } else {
+            cart.push({
+                name: dessertName,
+                price: dessertPrice,
+                quantity: 1
+            });
+        }
+
+         // Writing values into the Cart
+
+         let cartHolder = document.querySelector('.cart-wrapper');
+
+
+         cartHolder.innerHTML = `<h2>Your Cart (${cart.length})</h2>
+                               <div class="added-items">
+                               </div>`;
+
+        let cartAddedItems = cartHolder.querySelector('.added-items');
+
+        cart.forEach(item => {
+            let div = document.createElement('div');
+
+            div.classList.add('order-item');
+
+            div.innerHTML += `
+                                <p>${item.name}</p>
+                                <span class="quantity">${item.quantity}x</span> 
+                                <span class="item-price">@${item.price}</span>
+                                 <span class="sum">${'$?'} </span>
+                           `;
+            cartAddedItems.append(div);
+
+            console.log(`Added ${item.name} to the cart!`);
+        });
+
     });
 });
 
 
-// Function to update look of the Cart
 
-function updateCartUI() {
-
-    // Writing values into the Cart
-
-    let cartItems = document.querySelector('.cart-wrapper');
-
-    cartItems.innerHTML = '';
-
-    cart.forEach(item => {
-        let p = document.createElement('p');
-        p.textContent = item;
-        cartItems.appendChild(p);
-    });
-
-}
-
-// Function for decrement
-
-function decrement() {
-    let decrease = document.querySelector('.decrement');
-    decrease.addEventListener()
-}
