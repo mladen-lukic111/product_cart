df52481485ef30da1ceb87bbf7870efbe8fec03e
Mladja <mladenlukic111@gmail.com>
2024-08-27 17:19:59 +0200

    Sum of the quantity and price functionality

--- js/script.js
+++ js/script.js
@@ -30,34 +30,45 @@ addToCartBtns.forEach( button => {
         }
 
          // Writing values into the Cart
-
-         let cartHolder = document.querySelector('.cart-wrapper');
-
-
-         cartHolder.innerHTML = `<h2>Your Cart (${cart.length})</h2>
-                               <div class="added-items">
-                               </div>`;
-
-        let cartAddedItems = cartHolder.querySelector('.added-items');
-
-        cart.forEach(item => {
-            let div = document.createElement('div');
-
-            div.classList.add('order-item');
-
-            div.innerHTML += `
-                                <p>${item.name}</p>
-                                <span class="quantity">${item.quantity}x</span> 
-                                <span class="item-price">@${item.price}</span>
-                                 <span class="sum">${'$?'} </span>
-                           `;
-            cartAddedItems.append(div);
-
-            console.log(`Added ${item.name} to the cart!`);
-        });
+         addItem();
 
     });
+
 });
 
+
+function addItem() {
+    let cartHolder = document.querySelector('.cart-wrapper');
+
+
+    cartHolder.innerHTML = `<h2>Your Cart (${cart.length})</h2>
+                          <div class="added-items">
+                          </div>`;
+
+   let cartAddedItems = cartHolder.querySelector('.added-items');
+
+   cart.forEach(item => {
+       let div = document.createElement('div');
+
+       div.classList.add('order-item');
+
+       // Parsing item price to number
+       nItemPrice = item.price.replace("$", "");
+       numberItemPrice = parseFloat(nItemPrice).toFixed(2);
+
+       sum = item.quantity * numberItemPrice;
+
+       div.innerHTML += `
+                           <p>${item.name}</p>
+                           <span class="quantity">${item.quantity}x</span> 
+                           <span class="item-price">@${item.price}</span>
+                            <span class="sum">${sum}</span>
+                      `;
+       cartAddedItems.append(div);
+
+       console.log(`Added ${item.name} to the cart!`);
+
+   });
+}
 
 
